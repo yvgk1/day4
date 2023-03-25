dbf0eefadd519b35ca4a65042fda7107c6fbeef7
yvgk1 <emretopal32@gmail.com>
2023-03-25 16:07:21 +0300

    homework1.py
    python selenium day4 homework1

---
+++ homework1.py
@@ -0,0 +1,27 @@
+# HTML
+# HTML Locators
+# Selenium'da aksiyonlar (send_keys, click vb..)
+
+# 1-HTML
+# (HyperText Markup Language) kelimelerinin baş harflerinin kısaltılmasıdır 
+# web sayfalarını oluşturmak için kullanılan standart metin işaretleme dilidir.
+
+# 2-HTML Locators
+# HTML locators web sayfalarını otomatik olarak test etmek için kullanılır.Bir test senaryosu
+# ve bir html locator test senaryosunda test edilecek senaryonun öğeyi bulmak için kullanılır.
+# Html locators Selenium gibi web otomasyon araçlarında sık sık kullanılır. Bu araçlar öğeyi bulmak 
+# ve öğe üzerinde işlem yapmak için kullanılır.
+
+# 3-Selenium'da aksiyonlar
+# Click: Bir öğeye tıklama işlemi gerçekleştirir.
+# Send Keys: Bir öğeye klavye girdisi gönderir.
+# Select: Bir listeden öğe seçme işlemi gerçekleştirir.
+# Drag and Drop: Bir öğeyi taşıma işlemi gerçekleştirir.
+# Hover: Fareyi bir öğenin üzerine getirir ve işlem yapmadan önce bekletir.
+# Clear: Bir öğenin içeriğini temizler.
+# Get Attribute: Bir öğenin belirli bir özelliğini (örneğin, "value" veya "href") alır.
+# Submit: Bir formu gönderme işlemi gerçekleştirir.
+
+# Bu aksiyonlar, bir web sayfasındaki öğeleri etkileşimli hale 
+# getirerek otomasyon senaryoları oluşturmak için kullanılabilir. Bu senaryolar, 
+# web uygulamalarının doğruluğunu ve güvenilirliğini test etmek için kullanılabilir.
